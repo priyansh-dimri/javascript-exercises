f93cda74558344ae1af775b951931c5fb4de17ee
Priyansh Dimri <priyanshd0908@gmail.com>
2024-10-18 15:11:43 +0530

    feat: complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let temperatureInCelsius = (temperatureInFahrenheit - 32) * (5 / 9);
+  // return +temperatureInCelsius.toFixed(1);
+  return Math.round(temperatureInCelsius * 10) / 10; // Rounding to 1 decimal digit
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  let temperatureInFahrenheit = (temperatureInCelsius * (9 / 5)) + 32;
+  // return +temperatureInFahrenheit.toFixed(1);
+  return Math.round(temperatureInFahrenheit * 10) / 10; // Rounding to 1 decimal digit
 };
 
 // Do not edit below this line
