16dc248f3cb4bc0ae15a64c66e23d14fa297a461
Priyansh Dimri <priyanshd0908@gmail.com>
2024-10-18 14:55:16 +0530

    feat: complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // start and end can only be of `number` data type
+    if(isNaN(start) || isNaN(end)) return "ERROR";
 
+    // Both start and end should be non-negative
+    if(start < 0 || end < 0) return "ERROR";
+
+    // Both start and end should be non-negative integers and not decimals
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR"
+
+    // If start is greater than end, we reverse their values
+    if(start > end) [start, end] = [end, start];
+
+    let sum = 0;
+
+    for(; start <= end; ++start) {
+        sum += start;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
