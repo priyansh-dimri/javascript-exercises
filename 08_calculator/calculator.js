04bdbd68497745a808b68baef3e654928e48f980
Priyansh Dimri <priyanshd0908@gmail.com>
2024-11-05 11:24:53 +0530

    feat: complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((total, num)=>{
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((product, num) => {
+    return product * num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1) return 1;
+
+  return num * factorial(num-1);
 };
 
 // Do not edit below this line
