c699c4a3242a943e527d0749601ab5bcf2bc657a
Priyansh Dimri <priyanshd0908@gmail.com>
2024-11-05 11:55:14 +0530

    feat: complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,38 @@
-const palindromes = function () {
+const isAlphaNum = (c) => {
+    return ( c >= 'a' && c <= 'z' ) || (c >= '0' && c <= '9');
+}
 
-};
+// const areEqualCharacters = (a, b) => {
+//     return a.toLowerCase() === b.toLowerCase();
+// }
+
+// const palindromes = function (str) {
+//     let len = str.length;
+//     let left = 0, right = len-1;
+
+//     while(left < right) {
+//         while(left < len && !isAlphabet(str[left])) ++left;
+//         while(right >= 0 && !isAlphabet(str[right])) --right;
+
+//         if(left >= right) break;
+
+//         if(!areEqualCharacters(str[left], str[right])) return false;
+
+//         ++left;
+//         --right;
+//     }
+
+//     return true;
+// };
+
+const palindromes = (string) => {
+    const cleaned = string.toLowerCase().split('').filter(isAlphaNum).join('');
+
+    const reversed = cleaned.split('').reverse().join('');
+
+    return reversed == cleaned;
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
